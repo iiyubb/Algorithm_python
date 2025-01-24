8d58be6bf216f713b76cbc5f0b2fc2d5d3698d66
leeyub <80386881+iiyubb@users.noreply.github.com>
2025-01-24 11:28:02 +0900

    [Silver III] Title: N과 M (5), Time: 216 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+in_arr = [int(i) for i in input().split()]
+
+arr = []
+
+def recur(num):
+    if (num == m):
+        print(*arr)
+        return
+    
+    for i in sorted(in_arr):
+        if i in arr:
+            continue
+        arr.append(i)
+        recur(num+1)
+        arr.pop()
+        
+recur(0)
